2802d4e2ab43270c09e980ac68e2109ea76c5784
Eywek <contact@eywek.fr>
2018-03-12 11:51:38 +0100

    chore: use ES6 (class) on PM2Client; tests: add tests on PM2Client

--- src/PM2Client.js
+++ src/PM2Client.js
@@ -8,7 +8,6 @@
 
 const axon = require('pm2-axon')
 const cst = require('../constants.js')
-const util = require('util')
 const rpc = require('pm2-axon-rpc')
 const log = require('debug')('pm2:interface')
 const EventEmitter = require('events').EventEmitter
@@ -19,84 +18,85 @@ const EventEmitter = require('events').EventEmitter
  * @param {String} opts.sub_port socket file of the PM2 bus [optionnal]
  * @param {String} opts.rpc_port socket file of the PM2 RPC server [optionnal]
  */
-var PM2Wrapper = function (opts) {
-  const subSocket = (opts && opts.sub_port) || cst.DAEMON_PUB_PORT
-  const rpcSocket = (opts && opts.rpc_port) || cst.DAEMON_RPC_PORT
+module.exports = class PM2Wrapper extends EventEmitter {
+  constructor (opts) {
+    super()
+    const subSocket = (opts && opts.sub_port) || cst.DAEMON_PUB_PORT
+    const rpcSocket = (opts && opts.rpc_port) || cst.DAEMON_RPC_PORT
 
-  var self = this
+    EventEmitter.call(this)
 
-  EventEmitter.call(this)
+    const sub = axon.socket('sub-emitter')
+    this.sub_sock = sub.connect(subSocket)
+    this.bus = sub
 
-  const sub = axon.socket('sub-emitter')
-  this.sub_sock = sub.connect(subSocket)
-  this.bus = sub
+    const req = axon.socket('req')
+    this.rpc_sock = req.connect(rpcSocket)
+    this.rpc_client = new rpc.Client(req)
 
-  const req = axon.socket('req')
-  this.rpc_sock = req.connect(rpcSocket)
-  this.rpc_client = new rpc.Client(req)
+    this.rpc = {}
 
-  this.rpc = {}
-
-  this.rpc_sock.on('connect', function () {
-    log('PM2 API Wrapper connected to PM2 Daemon via RPC')
-    self.emit('rpc_sock:ready')
-    generateMethods(function () {
-      self.emit('ready')
+    this.rpc_sock.on('connect', _ => {
+      log('PM2 API Wrapper connected to PM2 Daemon via RPC')
+      this.emit('rpc_sock:ready')
+      this.generateMethods(_ => {
+        this.emit('ready')
+      })
     })
-  })
 
-  this.rpc_sock.on('close', function () {
-    log('rpc_sock:closed')
-    self.emit('close')
-  })
+    this.rpc_sock.on('close', _ => {
+      log('rpc_sock:closed')
+      this.emit('rpc_sock:closed')
+    })
 
-  this.rpc_sock.on('reconnect attempt', function () {
-    log('rpc_sock:reconnecting')
-    self.emit('reconnecting')
-  })
+    this.rpc_sock.on('reconnect attempt', _ => {
+      log('rpc_sock:reconnecting')
+      this.emit('rpc_sock:reconnecting')
+    })
 
-  this.sub_sock.on('connect', function () {
-    log('sub_sock ready')
-    self.emit('sub_sock:ready')
-  })
+    this.sub_sock.on('connect', _ => {
+      log('sub_sock ready')
+      this.emit('sub_sock:ready')
+    })
 
-  this.sub_sock.on('close', function () {
-    log('sub_sock:closed')
-    self.emit('closed')
-  })
+    this.sub_sock.on('close', _ => {
+      log('sub_sock:closed')
+      this.emit('sub_sock:closed')
+    })
 
-  this.sub_sock.on('reconnect attempt', function () {
-    log('sub_sock:reconnecting')
-    self.emit('reconnecting')
-  })
+    this.sub_sock.on('reconnect attempt', _ => {
+      log('sub_sock:reconnecting')
+      this.emit('sub_sock:reconnecting')
+    })
+  }
 
   /**
    * Disconnect socket connections. This will allow Node to exit automatically.
    * Further calls to PM2 from this object will throw an error.
    */
-  this.disconnect = function () {
-    self.sub_sock.close()
-    self.rpc_sock.close()
+  disconnect () {
+    this.sub_sock.close()
+    this.rpc_sock.close()
   }
 
   /**
    * Generate method by requesting exposed methods by PM2
    * You can now control/interact with PM2
    */
-  var generateMethods = function (cb) {
+  generateMethods (cb) {
     log('Requesting and generating RPC methods')
-    self.rpc_client.methods(function (err, methods) {
+    this.rpc_client.methods((err, methods) => {
       if (err) return cb(err)
-      Object.keys(methods).forEach(function (key) {
-        var method = methods[key]
+      Object.keys(methods).forEach((key) => {
+        let method = methods[key]
 
         log('+-- Creating %s method', method.name);
 
-        (function (name) {
-          self.rpc[name] = function () {
-            var args = Array.prototype.slice.call(arguments)
+        ((name) => {
+          this.rpc[name] = _ => {
+            let args = Array.prototype.slice.call(arguments)
             args.unshift(name)
-            self.rpc_client.call.apply(self.rpc_client, args)
+            this.rpc_client.call.apply(this.rpc_client, args)
           }
         })(method.name)
       })
@@ -104,7 +104,3 @@ var PM2Wrapper = function (opts) {
     })
   }
 }
-
-util.inherits(PM2Wrapper, EventEmitter)
-
-module.exports = PM2Wrapper
